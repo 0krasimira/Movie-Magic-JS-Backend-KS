af9e45180824cac9c45a7e1807831d1f80bec8a0
0krasimira <kstoyanova2@gmail.com>
2024-01-29 10:56:17 +0200

    Added post action for cast

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -4,4 +4,11 @@ castRouter.get("/create", (req, res) => {
     res.render("cast/create")
 })
 
+castRouter.post("/create", (req, res) => {
+    const body = req.body
+console.log(body)
+    res.redirect("/")
+})
+
+
 module.exports = castRouter
