89dd964560f90e502290d6ba06c2488b06ad00ea
0krasimira <kstoyanova2@gmail.com>
2024-01-30 11:40:09 +0200

    Error validation (try / catch) added to movie controller

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -20,28 +20,52 @@ movieRouter.post("/movies/create", async (req, res) => {
 })
 
 movieRouter.get('/movies/:movieId/details', async (req, res) => {
-    const movieId = req.params.movieId
+    try{
+        const movieId = req.params.movieId
     let movie = await movieManager.getOne(movieId).lean()
     // const casts = await castManager.getByIds(movie.casts).lean() ===> only if populate is not used - populate populates the cast info into the movie with the ref: Cast in the Movie Schema
     res.render('details', {movie})
+    } catch (error) {
+        console.log(error.message)
+        res.redirect("/")
+    }
+    
 })
 
 movieRouter.get('/search', async (req, res) => {
+    try{
     const {title, genre, year} = req.query
     const movies = await movieManager.search(title, genre, year).lean()
     res.render('search', {movies, title, genre, year})
+    } catch (error) {
+        console.log(error)
+        res.redirect("/")
+    }
+    
 })
 
 movieRouter.get('/movies/:movieId/attach', async (req, res) => {
-    const movie = await movieManager.getOne(req.params.movieId).lean()
-    const casts = await castManager.getAll().lean()
-    res.render('movie/attach', {...movie, casts})
+    try{
+        const movie = await movieManager.getOne(req.params.movieId).lean()
+        const casts = await castManager.getAll().lean()
+        res.render('movie/attach', {...movie, casts})
+    } catch (error) {
+        console.log(error)
+        res.redirect("/")
+    }
+    
 })
 
 movieRouter.post('/movies/:movieId/attach', async (req, res) => {
-    const castId = req.body.cast
-    await movieManager.attach(req.params.movieId, castId)
-    res.redirect(`/movies/${req.params.movieId}/attach`) // /movies/:movieId/attach
+    try{
+        const castId = req.body.cast
+        await movieManager.attach(req.params.movieId, castId)
+        res.redirect(`/movies/${req.params.movieId}/attach`) // /movies/:movieId/attach
+    } catch (error) {
+        console.log(error)
+        res.redirect("/")
+    }
+    
 })
 
 module.exports = movieRouter
