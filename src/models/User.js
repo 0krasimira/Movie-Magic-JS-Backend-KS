317b4528b989c3a2ef9be2c044b270162259ac01
0krasimira <kstoyanova2@gmail.com>
2024-02-05 10:00:59 +0200

    Hash password pre save

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose")
+const bcrypt = require("bcrypt")
 
 
 const userSchema = new Schema({
@@ -13,6 +14,13 @@ const userSchema = new Schema({
     }
 })
 
+userSchema.pre('save', async function() {
+    const hash = await bcrypt.hash(this.password, 12)
+    this.password = hash
+})
+
 const User = model("User", userSchema)
 
 module.exports = User
+
+
